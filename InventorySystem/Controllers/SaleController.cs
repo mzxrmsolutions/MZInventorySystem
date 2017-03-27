af4c25d19d058295a535628c501df1e887c6c6e4
ZeeshanShafqat <zeeshan.shafqat@aspose.com>
2017-03-27 18:12:22 +0500

    Sales Mockup

---
+++ InventorySystem/Controllers/SaleController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InventorySystem.Controllers
+{
+    public class SaleController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Sales";
+
+            return View();
+        }
+    }
+}
